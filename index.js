3931c8bc3a925a63ef7450accc9593c83e169c08
telmo-bidart <telmobidart@gmail.com>
2023-02-05 00:57:04 +0000

    added collision

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ class Boundary {
     this.width = 48
     this.height = 48
   }
+   
   draw() { 
     c.fillStyle = 'red'
     c.fillRect(this.position.x, this.position.y, this.width, this.height)
@@ -24,15 +25,21 @@ class Boundary {
 }
 
 const boundaries = []
+const offset = {
+  x: -1040,
+  y: -500
+}
+  
 collisionMap.forEach((row, i) => {
    row.forEach((symbol, j) => {
-    if (symbol === 110 )
+    if (symbol === 110)
     boundaries.push(new Boundary({position:{
-        x: j * Boundary.width,
-        y: i * Boundary.height
+        x: j * Boundary.width + offset.x,
+        y: i * Boundary.height + offset.y
     }}))
    })
 }) 
+
 
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
@@ -49,14 +56,26 @@ class Sprite {
       this.image = image
   }
   draw() { 
-    c.drawImage(this.image, this.position.x, this.position.y)
+    c.drawImage(
+      this.image,
+      0,
+      0,
+      this.image.width / 1,
+      this.image.height,
+        canvas.width / 2 - this.image.width / 2,
+        canvas.height / 2 - this.image.height / 2,
+        this.image.width / 4,
+        this.image.height)
   }
 }
  
 
-const background =  new Sprite({position: {
-  x: -1040,
-  y: -500},
+
+const background =  new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y,
+}, 
   image: image
 })
 
@@ -75,27 +94,41 @@ const keys = {
   }
 }
 
+const testBoundary = new Boundary({
+  position: {
+    x: 400,
+    y: 400
+  }
+})
+
+const movables = [background, testBoundary]
+
   function animate() {
     window.requestAnimationFrame(animate)
     background.draw()
-    boundaries.forEach(boundary => {
-      boundary.draw()
-    })
-  c.drawImage(
-    playerImage,
-    0,
-    0,
-    playerImage.width / 4,
-    playerImage.height,
-      canvas.width / 2 - playerImage.width / 2,
-      canvas.height / 2 - playerImage.height / 2,
-      playerImage.width / 4,
-      playerImage.height)
+     // boundaries.forEach(boundary => {
+      //boundary.draw();
+    //})
+    testBoundary.draw()
 
-      if (keys.w.pressed && lastKey === 'w') {background.position.y += 3}
-      else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
-      else if (keys.s.pressed && lastKey === 's') background.position.y -= 3
-      else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3
+//if (player.position.x + player.width)
+
+      if (keys.w.pressed && lastKey === 'w') {
+        movables.forEach(movables => 
+          {movables.position.y += 3
+        })}
+      else if (keys.a.pressed && lastKey === 'a') {
+        movables.forEach(movables => 
+        {movables.position.x += 3
+      })}
+      else if (keys.s.pressed && lastKey === 's') {
+        movables.forEach(movables => 
+        {movables.position.y -= 3
+      })}
+      else if (keys.d.pressed && lastKey === 'd') {
+        movables.forEach(movables => 
+        {movables.position.x -= 3
+      })}
     }
   animate()
 
